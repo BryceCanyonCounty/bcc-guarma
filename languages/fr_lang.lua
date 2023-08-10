4435750be7dfc60432c00ef83888555269154ba2
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-08-09 22:35:42 -0700

    Create fr_lang.lua
    - French translation by JackTask_AnTo

---
+++ languages/fr_lang.lua
@@ -0,0 +1,16 @@
+---------------------------------- FRENCH -------------------------------------------
+Locales["fr_lang"] = {
+    buyPrompt = "Acheter un billet : ~t6~25$",
+    travelPrompt = "Embarquer sur le bateau",
+    closedPrompt = "Voir les horaires",
+    closed = " ~o~: ~e~FERMÉ",
+    to = ":00~q~ à ~o~",
+    hundred = ":00",
+    hours = " est ouvert de ~o~",
+    shortCash = "Désolé, vous n'avez pas assez d'argent",
+    needJob = "Vous n'avez pas le poste requis !",
+    boughtTicket = "Merci pour votre achat !",
+    noTicket = "Vous avez besoin d'un billet pour monter à bord du bateau",
+    traveling = "Voyage vers ",
+    maxTickets = "Vous ne pouvez transporter que ~o~4~q~ billets"
+}
