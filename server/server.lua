ae39fd9d3b44116db1da90cf9e76ef13e43c443c
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-10-05 15:38:51 -0700

    Update server.lua
    - update vorp_inventory api
    - fix CheckPlayerJob callback name

--- server/server.lua
+++ server/server.lua
@@ -1,21 +1,19 @@
 local VORPcore = {}
-local VORPInv = {}
 
 TriggerEvent('getCore', function(core)
     VORPcore = core
 end)
-VORPInv = exports.vorp_inventory:vorp_inventoryApi()
 
 -- Check Ticket Qty on Player and Buy Ticket if Below Max
 RegisterNetEvent('bcc-guarma:BuyTicket', function(data)
     local src = source
     local Character = VORPcore.getUser(src).getUsedCharacter
     local buyPrice = data.buyPrice
-    local canCarry = VORPInv.canCarryItem(src, 'boat_ticket', 1)
+    local canCarry = exports.vorp_inventory:canCarryItem(src, 'boat_ticket', 1)
     if canCarry then
         if Character.money >= buyPrice then
             Character.removeCurrency(0, buyPrice)
-            VORPInv.addItem(src, 'boat_ticket', 1)
+            exports.vorp_inventory:addItem(src, 'boat_ticket', 1)
             VORPcore.NotifyRightTip(src, _U('boughtTicket'), 4000)
         else
             VORPcore.NotifyRightTip(src, _U('shortCash'), 4000)
@@ -28,9 +26,9 @@ end)
 -- If Player has Ticket, Take it and Send to Destination
 RegisterNetEvent('bcc-guarma:TakeTicket', function(data)
     local src = source
-    local ticket = VORPInv.getItem(src, 'boat_ticket')
+    local ticket = exports.vorp_inventory:getItem(src, 'boat_ticket')
     if ticket then
-        VORPInv.subItem(src, 'boat_ticket', 1)
+        exports.vorp_inventory:subItem(src, 'boat_ticket', 1)
         TriggerClientEvent('bcc-guarma:SendPlayer', src, data.location)
     else
         VORPcore.NotifyRightTip(src, _U('noTicket'), 4000)
@@ -38,7 +36,7 @@ RegisterNetEvent('bcc-guarma:TakeTicket', function(data)
 end)
 
 -- Check if Player has Required Job
-VORPcore.addRpcCallback('CheckPlayerJob', function(source, cb, shop)
+VORPcore.addRpcCallback('bcc-guarma:CheckPlayerJob', function(source, cb, shop)
     local src = source
     local Character = VORPcore.getUser(src).getUsedCharacter
     local playerJob = Character.job
