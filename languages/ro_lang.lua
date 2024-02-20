aecaff94c0d6b7690b6622ab5d64a6d7d40f6b51
iseeyoucopy <2729972+iseeyoucopy@users.noreply.github.com>
2024-02-20 10:33:35 +0000

    Create ro_lang.lua
    Added Romanian language support (ro_lang)

---
+++ languages/ro_lang.lua
@@ -0,0 +1,13 @@
+Locales["ro_lang"] = {
+    buyPrompt = "Cumpara Bilet: ~t6~$25",
+    travelPrompt = "Imbarca-te pe Nava",
+    to = ":00~q~ pâna la ~o~",
+    hundred = ":00",
+    hours = " este deschis de la ~o~",
+    shortCash = "Scuze, nu ai suficienti bani",
+    needJob = "Nu ai job-ul necesar!",
+    boughtTicket = "Multumim pentru achizitie!",
+    noTicket = "Ai nevoie de un bilet pentru a te imbarca pe nava",
+    traveling = "Calatoresti catre ",
+    maxTickets = "Poti purta doar ~o~4~q~ bilete"
+}
