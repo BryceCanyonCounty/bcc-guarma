2753562ae6c7e43cffa3f7924cb42304dbec0320
Quiteck1983 <53975057+Quiteck1983@users.noreply.github.com>
2024-12-09 18:56:33 +0100

    Create de_lang.lua

---
+++ languages/de_lang.lua
@@ -0,0 +1,14 @@
+---------------------------------- Deutsch -------------------------------------------
+Locales["de_lang"] = {
+    buyPrompt = "Ticket kaufen: ~t6~$",
+    travelPrompt = "Schiff betreten",
+    to = ":00~q~ nach ~o~",
+    hundred = ":00",
+    hours = " is open from ~o~",
+    shortCash = "Du hast nicht genug Geld",
+    needJob = "Aktuell findet kein Ticket-Verkauf statt!",
+    boughtTicket = "Danke für den Kauf!",
+    noTicket = "Du brauchst ein Ticket, um das Boot zu betreten",
+    traveling = "Reise nach ",
+    maxTickets = "Du kannst nur ~o~4~q~ Tickets bei dir haben"
+}
