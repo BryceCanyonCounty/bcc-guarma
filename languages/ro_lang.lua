be8065ca0c0b0ded6a9bc99cbbbffe20eefcc7fa
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-12-15 00:30:42 -0800

    Update ro_lang.lua

--- languages/ro_lang.lua
+++ languages/ro_lang.lua
@@ -1,10 +1,12 @@
 Locales["ro_lang"] = {
-    buyPrompt = "Cumpara Bilet: ~t6~$",
+    buyPrompt = "Costul Biletului: ",
     travelPrompt = "Imbarca-te pe Nava",
     to = ":00~q~ pâna la ~o~",
     hundred = ":00",
     hours = " este deschis de la ~o~",
     shortCash = "Scuze, nu ai suficienti bani",
+    shortGold = "Sorry, you don't have enough gold",
+    shortItem = "Sorry, you don't have enough items",
     needJob = "Nu ai job-ul necesar!",
     boughtTicket = "Multumim pentru achizitie!",
     noTicket = "Ai nevoie de un bilet pentru a te imbarca pe nava",
